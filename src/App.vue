928375aa2b86f092f5225d9c900cb31458d1b346
Oleg Shevchenko <oleg.shevchenko@mbunity.com>
2021-06-08 17:35:14 +0300

    vue cli lesson

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="Todo">
+        <h1>Your actions</h1>
+        <app-action 
+            v-for="(action,i) in todoList"
+            v-bind:title="action.title"
+            v-bind:current="action.current"
+            v-bind:max="action.max"
+            v-bind:key="i"
+            v-on:step="onNext(i)"
+            ></app-action>
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppAction from '@/components/Action.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        AppAction
+    },  
+    data: () => ({
+
+        todoList: [
+            { title: 'First action', current: 1, max: 5 },
+            { title: 'Other work', current: 2, max: 10 },
+            { title: 'Some fun', current: 4, max: 7 }
+        ]
+    }),
+    methods: {
+        onNext(i){
+            this.todoList[i].current++;
+        },
+
+    }
+	
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
